18a98717a79c0d429121da0bf7dd489a7f771373
lyusk8 <lyusk8@gmail.com>
2021-07-25 12:08:26 -0300

    Implementados metodos traidoresReport e rebeldesReport

--- src/main/java/br/com/letscode/starwarsresistancesocialnetwork/relatorio/RelatorioService.java
+++ src/main/java/br/com/letscode/starwarsresistancesocialnetwork/relatorio/RelatorioService.java
@@ -1,11 +1,13 @@
 package br.com.letscode.starwarsresistancesocialnetwork.relatorio;
 
 import br.com.letscode.starwarsresistancesocialnetwork.rebelde.Rebelde;
+import br.com.letscode.starwarsresistancesocialnetwork.rebelde.RebeldeRepository;
 import br.com.letscode.starwarsresistancesocialnetwork.rebelde.RebeldeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,33 +15,38 @@ public class RelatorioService {
 
     final RebeldeService rebeldeService;
 
+
     public String traidoresReport() throws IOException {
         List<Rebelde> listaRebeldes = rebeldeService.listAll();
         var qtdRebeldes = listaRebeldes.size();
-//        var qtdTraidores = listaTraidores.size();
-        //TODO listaTraidores.size();
-        //TODO var traidoresPercent = (qtdTraidores*100) / (qtdRebeldes+qtdTraidores)
-//        return String.format("Porcentagem de traidores: %d%", traidoresPercent);
-        return null;
+        List<Rebelde> listaTraidores = listaRebeldes.stream()
+                .filter(rebelde -> rebelde.isTraitor()).collect(Collectors.toList());
+        var qtdTraidores = listaTraidores.size();
+        var traidoresPercent = (qtdTraidores*100)/(qtdTraidores);
+        return String.format("Porcentagem de traidores: %d", traidoresPercent);
     }
 
     public String rebeldesReport() throws IOException {
         List<Rebelde> listaRebeldes = rebeldeService.listAll();
         var qtdRebeldes = listaRebeldes.size();
-//        var qtdTraidores = listaTraidores.size();
-        //TODO listaTraidores.size();
-        //TODO var rebeldePercent = (qtdRebeldes*100) / (qtdRebeldes+qtdTraidores)
-//        return String.format("Porcentagem de rebeldes: %d%", rebeldePercent);
-        return null;
+        List<Rebelde> listaTraidores = listaRebeldes.stream()
+                .filter(rebelde -> rebelde.isTraitor()).collect(Collectors.toList());
+        var qtdTraidores = listaTraidores.size();
+        var rebeldePercent = ((qtdRebeldes - qtdTraidores)*100) / (qtdRebeldes);
+        return String.format("Porcentagem de rebeldes: %d", rebeldePercent);
     }
 
-    public String pontosReport(){
+    public String pontosReport() throws IOException {
+        List<Rebelde> listaRebeldes = rebeldeService.listAll();
+//        List<Rebelde> listaTraidores = listaRebeldes.stream()
+//                .filter(rebelde -> rebelde.getQtdReport() >= 3).collect(Collectors.toList());
+//        listaTraidores.stream().map(traidor -> traidor.getInventario().size();
         //TODO pontos perdidos devido traidores
         return null;
     }
 
     public String recursosReport(){
-        //TODO qtd media de recursos por item (calculo feito sem a contagem dos itens de triadores)
+        //TODO qtd media de recursos por item (calculo feito sem a contagem dos itens de traidores)
         return null;
     }
 }
